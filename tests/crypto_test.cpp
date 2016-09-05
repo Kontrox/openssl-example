f39069c4f5fc838e4ee3bab985907852aa51b58e
Fredrik <fredrms@student.ntnu.no>
2016-09-05 16:23:21 +0200

    Ferdig oving

--- tests/crypto_test.cpp
+++ tests/crypto_test.cpp
@@ -1,8 +1,12 @@
 #include "crypto.hpp"
 #include <cassert>
+#include <string.h>
+#include <iostream>
 
 using namespace std;
 
+bool force(string hash, string salt, int iterations, int key_length);
+bool loop(char pswd[], int loops, string hash, string salt, int iterations, int key_length);
 int main() {
   //MD5
   assert(Crypto::hex(Crypto::md5("Hei123")) == "91ae3539ed65d862f7cb841fdb332911");
@@ -21,5 +25,65 @@ int main() {
   // Results copied from http://www.freecodeformat.com/pbkdf2.php
   assert(Crypto::hex(Crypto::pbkdf2("Password", "Salt", 4096, 128 / 8)) == "f66df50f8aaa11e4d9721e1312ff2e66");
   assert(Crypto::hex(Crypto::pbkdf2("Password", "Salt", 8192, 512 / 8)) == "a941ccbc34d1ee8ebbd1d34824a419c3dc4eac9cbc7c36ae6c7ca8725e2b618a6ad22241e787af937b0960cf85aa8ea3a258f243e05d3cc9b08af5dd93be046c");
-  assert(Crypto::hex(Crypto::pbkdf2("hash123", "Saltet til Ola", 2048)) == "ab29d7b5c589e18b52261ecba1d3a7e7cbf212c6" );
+  assert(Crypto::hex(Crypto::pbkdf2("QwE", "Saltet til Ola", 2048, 160 / 8)) == "ab29d7b5c589e18b52261ecba1d3a7e7cbf212c6" );
+  //assert(force("ab29d7b5c589e18b52261ecba1d3a7e7cbf212c6", "Saltet til Ola", 2048, 128 / 8));
+  return 0;
+}
+
+
+/*bool force(string hash, string salt, int iterations, int key_length){
+  int pswdlen = 3;
+  char pswd[pswdlen] =;
+  for(int i=0; i<=pswdlen; ++i){
+    if(loop(pswd, i, hash, salt, iterations, key_length))
+      return true;
+  }
+  
+  return false;
+}
+
+bool loop(char pswd[], int loops, string hash, string salt, int iterations, int key_length){
+  for(char i=48; i<123; ++i){
+    pswd[loops] = i;
+    cout << pswd << endl;
+    if(Crypto::hex(Crypto::pbkdf2(pswd,salt, iterations, key_length)) == hash){
+      cout << pswd << endl;
+      return true;
+    }
+  }
+  return false;
+}*/
+bool force(string hash, string salt, int iterations, int key_length){
+  char pswd[4];
+  for(char i=48; i<123; ++i){
+    pswd[0] = i;
+    cout << pswd << endl;
+    if(Crypto::hex(Crypto::pbkdf2(pswd,salt, iterations, key_length)) == hash){
+      cout << pswd << endl;
+      return true;
+    }
+    for(char j=48; j<123; ++j){
+      pswd[1] = j;
+      if(Crypto::hex(Crypto::pbkdf2(pswd,salt, iterations, key_length)) == hash){
+        cout << pswd << endl;
+        return true;
+      }
+      for(char k=48; k<123; ++k){
+        pswd[2] = k;
+        if(Crypto::hex(Crypto::pbkdf2(pswd,salt, iterations, key_length)) == hash){
+          cout << pswd << endl;
+          return true;
+        }
+        for(char l=48; l<123; ++l){
+          pswd[3] = l;
+          if(Crypto::hex(Crypto::pbkdf2(pswd,salt, iterations, key_length)) == hash){
+            cout << pswd << endl;
+            return true;
+          }
+        }
+      }
+    }
+  }
+  
+  return false;
 }
